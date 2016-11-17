8e38f73e4ff779d008578337041ff39b14971ed5
Todd Isenstadt <todd.isenstadt@gmail.com>
2016-11-17 21:22:57 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user #This handles instances where the user needs "friendly forwarding" to the intended page. The user show page is a default if we're not looking to be forwarded to another intended page.
     else
      flash.now[:danger] = 'Invalid email/password combination'
      render 'new'
